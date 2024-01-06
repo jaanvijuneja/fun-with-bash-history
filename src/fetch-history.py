955182477dc4200bcbcb9429fd56ea7233c37f0e
jaanvi <junejajaanvi@gmail.com>
2024-01-06 14:13:01 -0500

    connected to sql and inserted history into the table version 1.0

--- src/fetch-history.py
+++ src/fetch-history.py
@@ -1,4 +1,10 @@
 import os
+import database_connection
+
+localhost = '127.0.0.1'
+username = 'root'
+password = 'Secret55'
+database = 'store_history_bash'
 
 history_file_path = os.path.expanduser('~/.bash_history')
 
@@ -9,4 +15,7 @@ if os.path.exists(history_file_path):
 
 
 for data in history:
-    print(data)
+    sql_querry = f"""
+    insert into user_history (history, user) VALUES (%s, 'jaanvi')
+    """
+    database_connection.insert_history(localhost, username, password, database, sql_querry, data)
